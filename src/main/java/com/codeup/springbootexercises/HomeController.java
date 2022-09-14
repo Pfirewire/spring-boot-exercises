31ca17c81681706f4449be126c12df4cd5b54eec
Stephen Pflug <stephen@pflug.us>
2022-09-14 16:44:00 -0500

    added code to catch if user tries to show a post with id that doesn't exist

--- src/main/java/com/codeup/springbootexercises/HomeController.java
+++ src/main/java/com/codeup/springbootexercises/HomeController.java
@@ -4,12 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    @ResponseBody
     public String landingPage() {
-        return "This is the landing page!";
+        return "posts/index";
     }
 }
