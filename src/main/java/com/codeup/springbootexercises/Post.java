fddfbbe8e0b0d206269d1ea61b8cd8f4ed6ae9eb
Stephen Pflug <stephen@pflug.us>
2022-09-16 11:23:23 -0400

    created user class with constructors and getters, setters

--- src/main/java/com/codeup/springbootexercises/Post.java
+++ src/main/java/com/codeup/springbootexercises/Post.java
@@ -6,46 +6,34 @@ import javax.persistence.*;
 @Table(name="posts")
 public class Post {
 
+
+    // private variables
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-
     @Column(nullable = false)
     private String title;
-
     @Column(nullable = false, columnDefinition = "TEXT")
     private String body;
 
-    public Post() {}
 
+    // constructor functions
+    public Post() {}
     public Post(String title, String body) {
         this.title = title;
         this.body = body;
     }
 
-    public Long getId() {
-        return this.id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    // getters and setters
+    public Long getId() { return this.id; }
+    public void setId(Long id) { this.id = id; }
 
-    public String getTitle() {
-        return this.title;
-    }
+    public String getTitle() { return this.title; }
+    public void setTitle(String title){ this.title = title; }
 
-    public void setTitle(String title){
-        this.title = title;
-    }
-
-    public String getBody() {
-        return this.body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
+    public String getBody() { return this.body; }
+    public void setBody(String body) { this.body = body; }
 
 
 }
---
+++ src/main/java/com/codeup/springbootexercises/User.java
@@ -0,0 +1,45 @@
+package com.codeup.springbootexercises;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    // private variables
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+
+
+    // constructor functions
+    public User () {}
+    public User (String email, String username, String password) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+
+    // getters and setters
+    public Long getId() { return this.id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getEmail () { return this.email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getUsername () { return this.username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword () { return this.password; }
+    public void setPassword(String password) { this.password = password; }
+
+
+
+}
