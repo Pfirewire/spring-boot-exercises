0546e5a1b3fc47a2add3dcf3fc2ab7eba20f1419
Stephen Pflug <stephen@pflug.us>
2022-11-15 14:10:19 -0600

    moved files over to different repo

--- src/main/java/com/codeup/springbootexercises/repositories/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.codeup.springbootexercises.repositories;
-
-import com.codeup.springbootexercises.models.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-    Post findByTitle(String title);
-}
--- src/main/java/com/codeup/springbootexercises/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.codeup.springbootexercises.repositories;
-
-import com.codeup.springbootexercises.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
